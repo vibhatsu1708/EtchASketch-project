82e766a68a64e0429a4027086829e4bc638c226c
Vedant Mistry <mistryvedant17@gmail.com>
2022-02-22 20:03:08 +0530

    new build of the etch a sketch project

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
-function createGrid (x) {
-    for (let rows=0; rows<x; rows++) {
-        for (let columns=0; columns<x; columns++) {
+//function to make the grid
+function createGrid (number) {
+    for (let rows=0; rows<number; rows++) {
+        for (let columns=0; columns<number; columns++) {
             $("#container").append("<div class='grid'></div>");
         };
     };
-    $(".grid").width(960/x);
-    $(".grid").height(960/x);
+    $(".grid").width(960/number);
+    $(".grid").height(960/number);
 };
-$(document).ready(function() {
-    createGrid(16);
 
-    $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-        });
+//function that clears the grid
+function clearGrid () {
+    $(".grid").remove();
+}
 
-    $(".newGrid").click(function() {
-        refreshGrid();
+//function that prompts the user to select the number of boxes in the grid.
+function refreshGrid () {
+    let new_number = window.prompt("Enter the number of boxes on each side: ");
+    clearGrid();
+    createGrid(new_number);
+};
 
-        $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-        });
-    });
-});
+//function to display a default 16 by 16 grid, whose boxes change color on hovering over them.
+$(document).ready(
+    function () {
+        createGrid(16);
 
-function 
+        $(".grid").mouseover(
+            function () {
+                $(this).css("background-color", "#272635");
+            }
+        );
+
+        $(".new-grid").click(
+            function () {
+                refreshGrid();
+
+                $(".grid").mouseover (
+                    function () {
+                        $(this).css("background-color", "#272635");
+                    }
+                );
+            }
+        );
+    }
+);
