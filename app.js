41d2be44bd80cb045b36852ff863c7ac9f76f5f7
Vedant Mistry <mistryvedant17@gmail.com>
2022-08-11 03:13:34 +0530

    Successlly completed displaying the grid without any bugs, and now working on the hover effect and the resetting the grid functionality for the project.

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
+// gave the page container a variable.
 const container = document.querySelector('#container');
+// created a grid variable to hold the grid cells in the webpage, and adding the class grid to the declared variable.
 const grid = document.createElement('div');
 grid.classList.add('grid');
 
-let number = 5;
+// window prompt will ask the user for the number of boxes to be displayed.
+let number = window.prompt("Enter a random number");
 
-for (let i=1; i<=number; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
 
-    for (let j=1; j<=number; j++) {
-        let box = document.createElement('div');
-        box.classList.add('box');
-        box.setAttribute('style', 'border: 2px solid black;  width: 20px; height: 20px');
-        container.appendChild(box);
+// function to create the grid
+function generateGrid (number) {
+    // measure to keep the number of boxes limited to 16 per row, to avoid overflow of boxes from the webpage.
+    if (number > 16) {
+        number = 16;
     }
-    container.appendChild(grid);
+
+    for (let i=1; i<=number; i++) {
+        // creating a row element to contain the boxes per row, and then adding the class row for each row, and appending the rows to the grid element.
+        let row = document.createElement('div');
+        row.classList.add('row');
+        grid.appendChild(row);
+    
+        // loop to create an equal number of boxes per row, adding the class of box to each box in the row, and then appending the box elements to the row.
+        for (let j=1; j<=number; j++) {
+            let box = document.createElement('div');
+            box.classList.add('box');
+            row.appendChild(box);
+        }
+        container.appendChild(grid);
+    }
 }
 
+// function regenerateGrid () {
+    
+// }
+
+// function to generate the grid when a window is loaded.
+window.onload = function () {
+    generateGrid(number);
+}
